a88b3f0c6094f2291e7bb8b6ce11a3af5f63a9a3
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-01-05 12:35:10 -0800

    added firebase

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
-        <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>Cinemify</h1>
+        <h1 className='text-yellow-400 text-4xl font-bold cursor-pointer'>Cinemify</h1>
         <div>
             <button className='text-white pr-4'>Sign In</button>
-            <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Sign Up</button>
+            <button className='bg-yellow-500 px-6 py-2 rounded cursor-pointer text-white'>Sign Up</button>
         </div>
     </div>
   )
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import Movie from './Movie';
 import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
 
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL, rowId }) => {
     const [movies, setMovies] = useState([])
 
     useEffect(() => {
@@ -12,19 +12,29 @@ const Row = ({ title, fetchURL }) => {
         })
     },[fetchURL])
 
-    console.log(movies)
+    // console.log(movies)
+    const slideLeft = () => {
+        var slider = document.getElementById('slider' + rowId)
+        slider.scrollLeft = slider.scrollLeft - 500;
+    };
+    const slideRight = () => {
+        var slider = document.getElementById('slider' + rowId)
+        slider.scrollLeft = slider.scrollLeft + 500;
+    };
 
   return (
     <div>
       <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
       <div className='relative flex items-center group'>
-        <MdChevronLeft className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
-            <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+        <MdChevronLeft
+        onClick={slideLeft} className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
+            <div id={'slider' +rowId} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
             {movies.map((item, id) => (
                 <Movie key={id} item={item} />
         ))}
         </div>
-        <MdChevronRight className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
+        <MdChevronRight
+        onClick={slideRight} className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
     </div>
     </div>
   );
