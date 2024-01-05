f9ad5229394d290fc87d01f78bf3e96b2f6db651
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-01-05 12:14:48 -0800

    row stuff

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -17,13 +17,14 @@ const Row = ({ title, fetchURL }) => {
   return (
     <div>
       <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
-      <div className='relative flex items-center'>
+      <div className='relative flex items-center group'>
+        <MdChevronLeft className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
             <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
             {movies.map((item, id) => (
                 <Movie key={id} item={item} />
         ))}
-
-    </div>
+        </div>
+        <MdChevronRight className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
     </div>
     </div>
   );
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -7,12 +7,12 @@ const Home = () => {
   return (
     <div>
         <Main />
+        <Row title='Now Playing' fetchURL={requests.requestNowPlaying} />
         <Row title='Upcoming' fetchURL={requests.requestUpcoming} />
         <Row title='Popular' fetchURL={requests.requestPopular} />
         <Row title='Trending' fetchURL={requests.requestTrending} />
         <Row title='Top Rated' fetchURL={requests.requestTopRated} />
         {/* change later/add */}
-        <Row title='Horror' fetchURL={requests.requestHorror} />
     </div>
   )
 }
