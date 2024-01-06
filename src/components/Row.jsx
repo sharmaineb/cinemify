aa4269a51bd27b3affd004ff1bc8c46b9c7a11dd
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-01-05 16:37:36 -0800

    can play movie trailers!!

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,43 +1,130 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import YouTube from 'react-youtube';
 import Movie from './Movie';
-import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
 
 const Row = ({ title, fetchURL, rowId }) => {
-    const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
+  const [trailerURL, setTrailerURL] = useState('');
 
-    useEffect(() => {
-        axios.get(fetchURL).then((response) => {
-            setMovies(response.data.results)
-        })
-    },[fetchURL])
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(fetchURL);
+        setMovies(response.data.results);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-    // console.log(movies)
-    const slideLeft = () => {
-        var slider = document.getElementById('slider' + rowId)
-        slider.scrollLeft = slider.scrollLeft - 500;
-    };
-    const slideRight = () => {
-        var slider = document.getElementById('slider' + rowId)
-        slider.scrollLeft = slider.scrollLeft + 500;
-    };
+    fetchData();
+  }, [fetchURL]);
+
+  const slideLeft = () => {
+    var slider = document.getElementById('slider' + rowId);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    var slider = document.getElementById('slider' + rowId);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = async (movie) => {
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/${movie.id}/videos?api_key=${process.env.REACT_APP_TMDB_API_KEY}`
+      );
+      
+      if (response.data.results.length > 0) {
+        setTrailerURL(response.data.results[0]?.key);
+      } else {
+        console.log('No trailers available for this movie');
+      }
+    } catch (error) {
+      console.error('Error fetching trailers:', error);
+    }
+  };
 
   return (
     <div>
       <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
       <div className='relative flex items-center group'>
         <MdChevronLeft
-        onClick={slideLeft} className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
-            <div id={'slider' +rowId} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
-            {movies.map((item, id) => (
-                <Movie key={id} item={item} />
-        ))}
+          onClick={slideLeft}
+          className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+          size={40}
+        />
+        <div id={'slider' + rowId} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+          {movies.map((item, id) => (
+            <div key={id} className='inline-block cursor-pointer relative p-2' onClick={() => handleClick(item)}>
+              <Movie item={item} />
+            </div>
+          ))}
         </div>
         <MdChevronRight
-        onClick={slideRight} className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
-    </div>
+          onClick={slideRight}
+          className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+          size={40}
+        />
+      </div>
+      {trailerURL && <YouTube videoId={trailerURL} opts={opts} />}
     </div>
   );
 };
 
 export default Row;
+
+
+// import axios from 'axios';
+// import React, { useEffect, useState } from 'react';
+// import Movie from './Movie';
+// import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
+
+// const Row = ({ title, fetchURL, rowId }) => {
+//     const [movies, setMovies] = useState([])
+
+//     useEffect(() => {
+//         axios.get(fetchURL).then((response) => {
+//             setMovies(response.data.results)
+//         })
+//     },[fetchURL])
+
+//     // console.log(movies)
+//     const slideLeft = () => {
+//         var slider = document.getElementById('slider' + rowId)
+//         slider.scrollLeft = slider.scrollLeft - 500;
+//     };
+//     const slideRight = () => {
+//         var slider = document.getElementById('slider' + rowId)
+//         slider.scrollLeft = slider.scrollLeft + 500;
+//     };
+
+//   return (
+//     <div>
+//       <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
+//       <div className='relative flex items-center group'>
+//         <MdChevronLeft
+//         onClick={slideLeft} className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
+//             <div id={'slider' +rowId} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+//             {movies.map((item, id) => (
+//                 <Movie key={id} item={item} />
+//         ))}
+//         </div>
+//         <MdChevronRight
+//         onClick={slideRight} className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
+//     </div>
+//     </div>
+//   );
+// };
+
+// export default Row;
--- src/components/pages/Account.jsx
+++ src/components/pages/Account.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import SavedShows from '../SavedShows'
 
 const Account =() => {
   return (
-    <div>Account</div>
+    <div>
+      <div className='w-full text-white'>
+      <img 
+      className='w-full h-[400px] object-cover' 
+      src='https://img.freepik.com/free-photo/movie-background-collage_23-2149876028.jpg' 
+      alt=''>
+      </img>
+      <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'>
+        <div className='absolute top-[20%] p-4 md:p-8'>
+          <h1 className='text-3xl md:text-5xl font-bold'>My Shows</h1>
+        </div>
+      </div>
+      </div>
+      <SavedShows />
+    </div>
   )
 }
 
