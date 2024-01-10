ea98c52ee4b3fd602c3f1b7bfe363c44d96cc2c1
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-01-09 16:01:26 -0800

    working through search function 😐

--- src/Requests.js
+++ src/Requests.js
@@ -1,6 +1,7 @@
 const key = process.env.REACT_APP_TMDB_API_KEY;
 
 const requests = {
+    requestSearch: `https://api.themoviedb.org/3/search/movie?query=YourSearchQuery&api_key=${key}&language=en-US&page=1`,
     requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
     requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
     requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -7,11 +7,13 @@ const Home = () => {
   return (
     <div>
         <Main />
+        <Row rowId='6' title='Search' fetchURL={requests.requestSearch} />
         <Row rowId='1' title='Now Playing' fetchURL={requests.requestNowPlaying} />
         <Row rowId='2' title='Upcoming' fetchURL={requests.requestUpcoming} />
         <Row rowId='3' title='Popular' fetchURL={requests.requestPopular} />
         <Row rowId='4' title='Trending' fetchURL={requests.requestTrending} />
         <Row rowId='5' title='Top Rated' fetchURL={requests.requestTopRated} />
+        
         {/* change later/add */}
     </div>
   )
